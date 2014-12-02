49763fa9e850ac60e7db0f4031c120ca3e3c5413
Ricky Pattillo <ricky.pattillo@gmail.com>
2014-12-01 19:47:54 -0500

    Initial Commit

---
+++ Quiz/Quiz/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Quiz
+//
+//  Created by Ricky Pattillo on 12/1/14.
+//  Copyright (c) 2014 Ricky Pattillo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Quiz/Quiz/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Quiz
+//
+//  Created by Ricky Pattillo on 12/1/14.
+//  Copyright (c) 2014 Ricky Pattillo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
